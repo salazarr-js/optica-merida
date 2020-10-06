9054c6ac0d9a0cf495e9a92872a153364ccbf9d9
Jose Salazar <jsalazar@cardlytics.com>
2020-10-06 03:41:59 -0300

    fix route

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
 import { Store } from '@ngxs/store';
 import { Product } from '@app/models/product';
 import { AddProduct } from '@app/core/store/cart';
+import { ROUTES_NAMES } from '@app/routes/routes';
 
 
 /** PRODUCT CARD COMPONENT */
@@ -21,23 +22,20 @@ export class ProductCardComponent implements OnInit {
   
   /** */
   ngOnInit(): void {
-
-    console.log("PRODUCT: ", this.product);
   }
 
-  /** */
+  /** RETURN THE DETAIL ROUTE URL */
   public get detailUrl() {
-    return ['/', 'product', this.product.id || 1];
+    return ['/', ROUTES_NAMES.PRODUCT, this.product.id || 1];
   }
 
-  /** */
+  /** RETURN THE TOTAL FINAL PRICE */
   public get finalPrice(): number {
     const p = this.product;
-
     return (1 - p.discount/100) * p.price;
   }
 
-  /** */
+  /** RETURN FIRST 2 IMAGES */
   public get images(): string[] {
     let images = [];
     for (let i of [0, 1]) {
@@ -49,7 +47,7 @@ export class ProductCardComponent implements OnInit {
     return images;
   }
 
-  /** */
+  /** ADD PRODUCT TO CART */
   addToCart(): void {
     this.store.dispatch( new AddProduct(this.product.id || 1) );
   }
