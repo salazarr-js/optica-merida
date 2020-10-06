495eb01da87be3065d0e8dc69244c57d3f832332
Jose Salazar <jsalazar@cardlytics.com>
2020-10-06 00:00:07 -0300

    remove footer, styles

--- src/app/core/components/base-layout/base-layout.component.ts
+++ src/app/core/components/base-layout/base-layout.component.ts
@@ -1,30 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-/** CONSTs & MODELS */
-import { ROUTES_NAMES } from '@routes/routes';
-
-/** */
+/** BASE LAYOUT*/
 @Component({
   selector: 'base-layout',
   templateUrl: './base-layout.component.html',
   styleUrls: ['./base-layout.component.scss']
 })
 export class BaseLayoutComponent implements OnInit {
-  public urls = {
-    home: ['/', ROUTES_NAMES.HOME ],
-    product: ['/', ROUTES_NAMES.PRODUCT, 1 ],
-  };
-
   /** */
   constructor( ) {
-    // this.isLoading = false;
   }
 
   /** */
   ngOnInit() {
-    // this.store.select( FlagStatusState ).subscribe(() => {
-    //   const isProgress = this.store.selectSnapshot( FlagStatusState );
-    //   this.isLoading = isProgress.progressBar;
-    // });
   }
 }
--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,10 +1,8 @@
 import { BaseLayoutComponent } from './base-layout/base-layout.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FooterComponent } from './footer/footer.component';
 
 /** GROUP/EXPORT ALL APP WIDE COMPONENTs */
 export const CORE_COMPONENTS = [
     BaseLayoutComponent,
-    NavBarComponent,
-    FooterComponent,
+    NavBarComponent
 ];
